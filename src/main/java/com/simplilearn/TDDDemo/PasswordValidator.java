59a1cfdf7cf296f3e2aae87d2e413f014bdb6462
Mohanad Alfadhli <mohaned1929@gmail.com>
2023-10-12 12:14:22 +0000

    My first funtional testing project demo

---
+++ src/main/java/com/simplilearn/TDDDemo/PasswordValidator.java
@@ -0,0 +1,20 @@
+package com.simplilearn.TDDDemo;
+
+public class PasswordValidator {
+	
+	public static boolean isValid(String password) {
+		
+		if(password.isEmpty()) {
+			return false;
+		}else {
+			if(password.length() >=5 && password.length()<=10) {
+				return true;
+				
+			}else {
+				return false;
+				
+			}
+		}
+	}
+
+}
---
+++ src/test/java/com/simplilearn/TDDDemo/AppTest.java
@@ -0,0 +1,23 @@
+package com.simplilearn.TDDDemo;
+
+import org.testng.Assert;
+import org.testing.annotations.Test;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@Test
+	Run | Debug
+	
+	public void testPassword() {
+		
+		//PasswordValidator validator = new PasswordValidator();
+		
+		boolean result = vaildator.isValid("PqR");
+		
+		Assert.assertEquals(true, result);
+		
+	}
+   
+}
